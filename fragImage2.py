ef703076b9a2716537e7ac437f050e397c845cb3
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-04-01 13:05:40 -0400

    planned downsample example

---
+++ fragImage2.py
@@ -0,0 +1,16 @@
+# Fragment/split an image into tiles (initially, 512x512)
+# Brygg Ullmer, Clemson University
+# Begun 2023-03-22
+
+from enoTiledImg import *
+import sys
+
+if len(sys.argv) < 4:
+  print("Please provide image source filename, downsample factor, and target directory as arguments"); sys.exit(-1)
+
+imgSrcFn, downsample, dirTargFn = sys.argv[1:3]
+eti = enoTiledImg()
+eti.downsample(int(downsample))
+eti.decomposImage(imgSrcFn, dirTargFn)
+
+### end ###
