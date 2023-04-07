ee89a1bb3c9bc9a6c13192cbda06cb43dd39391f
Brygg Ullmer <brygg@60.2.23.172.generic.clemson.edu>
2023-04-07 00:30:11 -0400

    table definitions

--- enoContent.sql
+++ enoContent.sql
@@ -2,28 +2,28 @@
 -- By Brygg Ullmer, Clemson University --
 -- Begun 2023-04-06                    --
 
-create table contentServerEntry(
+create table contentServerEntry (
   id          integer unique primary key,
   fullAddress text,   -- e.g., 'https://enodia.computing.clemson.edu/'
   abbrev      text,   -- e.g., 'ecce'
   abbrevCount integer -- e.g., 1
-;
+);
 
-create table contentServerAbbrev(
+create table contentServerAbbrev (
   id     integer unique primary key,
   abbrev text,
   count  integer
 );
 
-create table spreadHead(
+create table spreadHead (
   id               integer unique primary key,
   contentSourceUrl text,
   relPath          text,
   numLevels        integer,
-  serverId         integer, -- contentServerEntry.id
+  serverId         integer -- contentServerEntry.id
 );
 
-create table spreadLevel(
+create table spreadLevel (
   id           integer unique primary key,
   spreadHeadId integer,
   level        integer,
