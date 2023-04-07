82a45e7559e334ef468976959f0add50fc7cac72
Brygg Ullmer <brygg@60.2.23.172.generic.clemson.edu>
2023-04-07 00:17:04 -0400

    initial tables

--- enoContent.sql
+++ enoContent.sql
@@ -3,6 +3,9 @@
 -- Begun 2023-04-06                    --
 
 create table enoContentEntry(
+  id          integer unique primary key,
+  fullAddress text,
+  abbrevId    integer
 ;
 
 create table enoContentAbbrev(
