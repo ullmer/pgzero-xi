902ce0ac037566be52213442fc8ca6b95ca65383
Brygg Ullmer <brygg@60.2.23.172.generic.clemson.edu>
2023-04-07 00:10:20 -0400

    stub

--- enoContent.sql
+++ enoContent.sql
@@ -2,4 +2,13 @@
 -- By Brygg Ullmer, Clemson University --
 -- Begun 2023-04-06                    --
 
+create table enoContentEntry(
+;
+
+create table enoContentAbbrev(
+  id     integer unique primary key,
+  abbrev text,
+  count  integer
+);
+
 --- end ---
