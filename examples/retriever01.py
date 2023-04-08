5ef757667c9d05d3b70800e3576bd60501a56a4b
Brygg Ullmer <brygg@183.2.23.172.generic.clemson.edu>
2023-04-08 09:24:12 -0400

    test of eno content retriever

---
+++ examples/retriever01.py
@@ -0,0 +1,16 @@
+#Test content retriever
+#Brygg Ullmer, Clemson University
+#https://enodia.computing.clemson.edu/tiled/loc/maps/sc/1970/1/tile00x00.png
+
+from enoContentRetriever import *
+
+ecr = enoContentRetriever()
+
+baseUrl = 'https://enodia.computing.clemson.edu/tiled/loc/maps/sc/1970/1/tile00x'
+
+for i in range(20):
+  url = '%s%02i.png' % (baseUrl, i)
+  fn  = 'tmp/%02i.png' % i
+  ecr.retrieveContent(url, fn, ecr.printUrlLoaded)
+
+### end ###
