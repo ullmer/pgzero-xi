9dc7c7e0ab4e98f40dbce023085f87ad7338af81
Brygg Ullmer <brygg@183.2.23.172.generic.clemson.edu>
2023-04-08 09:29:53 -0400

    example progress

--- examples/retriever01.py
+++ examples/retriever01.py
@@ -11,6 +11,12 @@ baseUrl = 'https://enodia.computing.clemson.edu/tiled/loc/maps/sc/1970/1/tile00x
 for i in range(20):
   url = '%s%02i.png' % (baseUrl, i)
   fn  = 'tmp/%02i.png' % i
+  print(url, fn)
   ecr.retrieveContent(url, fn, ecr.printUrlLoaded)
+
+with ecr.executor:
+  ecr.checkResults()  
+  print(".")
+  sleep(100)
 
 ### end ###
