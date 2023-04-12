d4d0d310100a3e48c44989b8321b0e74fe704449
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-04-12 09:39:02 -0400

    Adding onboard neopixel toggle

--- examples/arduino/arduinoEx02/arduinoEx02.ino
+++ examples/arduino/arduinoEx02/arduinoEx02.ino
@@ -3,21 +3,42 @@
 // Support by NSF CNS-1828611
 // Begun 2023-04
 
+#define LED0 13
+//#define BAUD 9600
+#define BAUD 115200
+#define LED_BRIGHTNESS 1
+int millisPerUpdate = 1000; //how many milliseconds should pass before each ~heartbeat update
+
+#include <Adafruit_NeoPixel.h>
+
+#define NUMPIXELS 1
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
 // https://learn.adafruit.com/adafruit-grand-central/adapting-sketches-to-m0
 #if defined(ARDUINO_SAMD_ZERO) && defined(SERIAL_PORT_USBVIRTUAL)
   // Required for Serial on Zero based boards
   #define Serial SERIAL_PORT_USBVIRTUAL
 #endif
 
-#define LED0 13
 
-int millisPerUpdate = 1000; //how many milliseconds should pass before each ~heartbeat update
+
 int millisLastUpdate = 0;
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
   pinMode(13, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(BAUD);
+
+  #if defined(NEOPIXEL_POWER)
+  // If this board has a power control pin, we must set it to output and high
+  // in order to enable the NeoPixels. We put this in an #if defined so it can
+  // be reused for other boards without compilation errors
+  pinMode(NEOPIXEL_POWER, OUTPUT);
+  digitalWrite(NEOPIXEL_POWER, HIGH);
+  #endif
+
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.setBrightness(LED_BRIGHTNESS); // not so bright
 }
 
 ////////////////// serialCount /////////////////////
@@ -30,15 +51,26 @@ void serialCount() {
 
 ////////////////// toggle LED0 /////////////////////
 
+int led0Activated = 0;
+int led1Activated = 1;
 int led0State = 0;
-void toggleLED0() {
-  if (led0State == 0) {digitalWrite(LED0, HIGH); led0State=1;}
+int led1State = 0;
+void toggleLED() {
+  if (led0Activated && led0State == 0) {digitalWrite(LED0, HIGH); led0State=1;}
   else               {digitalWrite(LED0, LOW);  led0State=0;}
+
+  if (led1Activated && led1State == 0) {
+    pixels.fill(0xFF0000);
+    pixels.show(); led1State=1;
+  } else {
+    pixels.fill(0x0000FF);
+    pixels.show(); led1State=0;
+  }
 }
 
 ////////////////// heartbeat /////////////////////
 void heartbeat() {
-  toggleLED0(); serialCount();
+  toggleLED(); serialCount();
 }
 
 ////////////////// serial read update /////////////////////
