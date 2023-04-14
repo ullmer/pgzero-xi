97ec7d828733f7d45c789d498a0148cc4b512d74
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-04-14 18:03:18 -0400

    control-c handled

--- examples/arduino/arduinoEx03/kbSerialRelayer.py
+++ examples/arduino/arduinoEx03/kbSerialRelayer.py
@@ -2,8 +2,8 @@
 
 #https://www.instructables.com/Python-Serial-Port-Communication-Between-PC-and-Ar/
 
-import serial
-import getch
+import signal, sys, time
+import serial, getch, msvcrt
 
 SerialObj = serial.Serial('COM5') # COMxx  format on Windows
 SerialObj.baudrate = 115200 # set Baud rate to 9600
@@ -11,10 +11,17 @@ SerialObj.bytesize = 8      # Number of data bits = 8
 SerialObj.parity   ='N'      # No parity
 SerialObj.stopbits = 1   # Number of Stop bits = 1
 
-while True:
-  ch = getch.getch()
-  SerialObj.write(ch) 
+def sigint_handler(sig, frame):
+  SerialObj.close()      # Close the port
+  sys.exit(0)
 
-SerialObj.close()      # Close the port
+signal.signal(signal.SIGINT, sigint_handler)
+
+while True:
+  if msvcrt.kbhit(): 
+    ch = getch.getch()
+    SerialObj.write(ch) 
+
+  time.sleep(1)
 
 ### end ###
