ab2deb1c6d96448c561746569e593bbd3047bf8f
Brygg Ullmer <ullmer@acm.org>
2023-04-17 08:35:06 -0400

    beginning to implement virtual function, that does the actual illumination.

--- examples/arduino/arduinoEx05/enoLedmapper.h
+++ examples/arduino/arduinoEx05/enoLedmapper.h
@@ -4,43 +4,18 @@
 // Begun 2023-04
 // LLGPL3
 
-#ifndef enoLEDmapper_h
-#define enoLEDmapper_h
+#ifndef enoLedmapper_h
+#define enoLedmapper_h
 #endif
 
-#define ECM_DEFAULT_LEDCHUNK 10
+#include "enoLightmapper.h"
 
-class enoLEDmapper {
+///////////////////////////// Enodia Light Mapper /////////////////////////////
+
+class enoLedmapper : public enoLightmapper {
   public:
-    enoLEDmapper(int numLEDs, int whichChain=0, int maxBrightVal=10);
-    void allocateLedBuffers(int numLEDs);
-    void registerLED(char ledKey, char ledColorKey, int ledIdx=-1, 
-                     const char ledName[]="", int ledBright=5); //-1 = next in sequence
-
-    char *getLEDSummaryStr();
-    char *getLEDYamlStr();
-
-    int getLEDColByKey(char ledKey);
-    int getLEDColByIdx(int  ledIdx);
-    int getLEDColByName(char *ledName, bool caseSensitive=false);
-    int getLEDIdxByName(char *ledName, bool caseSensitive=false);
-    
-    int setLEDByKey(char ledKey, char ledColor, int ledBrightness);
-    int setLEDByIdx(int  ledIdx, char ledColor, int ledBrightness);
-    int setLEDByName(char *ledName, char ledColor, int ledBrightness, bool caseSensitive=false);
-    
-  private:
-    int  whichChain;
-    char *ledKeys      = NULL;
-    char *ledColorKeys = NULL;
-    char *ledBright    = NULL;
-    const char **ledNames; // may benefit from revisiting
-    int numLEDs;
-    int ledCursorIdx  = 0;
-    int maxLEDNameLen = 15; 
-    int maxBrightVal  = 10; 
-    int maxCharsPerLine = 82; //80 + CRLF
-    enoLEDmapper *nextLEDmapperChunk;
+    enoLedmapper(int numLights, int whichChain=0, int maxBrightVal=10);
+    void setLightByIdx(int  lightIdx, char lightColor, int lightBrightness);
 };
 
-// end //
+/// end ///
