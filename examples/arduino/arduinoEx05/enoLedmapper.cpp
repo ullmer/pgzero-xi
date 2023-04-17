cb2d74052bf513b7bab23ab59108941dad57eca7
Brygg Ullmer <brygg@Bryggs-Mac-mini.local>
2023-04-17 09:18:17 -0400

    steps toward populating led calls

--- examples/arduino/arduinoEx05/enoLedmapper.cpp
+++ examples/arduino/arduinoEx05/enoLedmapper.cpp
@@ -31,86 +31,21 @@ void enoLedmapper::initLeds() {
   pixels.setBrightness(7); // not so bright
 }
 
+///////////////////////////// set light by idx /////////////////////////////
 
-    void setLightByIdx(int  lightIdx, char lightColor, int lightBrightness);
-    void showLight();
+void enoLedmapper::setLightByIdx(int  lightIdx, char lightColor, 
+   int lightBrightness) {
 
- pixels.setPixelColor(pixelNumber,
-   pgm_read_byte(&gamma8[r]),
-   pgm_read_byte(&gamma8[g]),
-   pgm_read_byte(&gamma8[b]));
- pixels.show();
-
-  private:
-    Adafruit_NeoPixel pixels;
-};
-
-//LED gamma table from https://cdn-learn.adafruit.com/downloads/pdf/led-tricks-gamma-correction.pdf
-
-const uint8_t PROGMEM gamma8[] = {
- 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
- 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
- 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2,
- 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5,
- 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10,
- 10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
- 17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
- 25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
- 37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
- 51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
- 69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
- 90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
- 115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
- 144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
- 177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
- 215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
-
- ///end///
-#include <Adafruit_NeoPixel.h>
-
-#define NUMPIXELS 1
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
-
-// the setup function runs once when you press reset or power the board
-void setup() {
-  
-
-  Serial.begin(9600);
+  pixels.setPixelColor(pixelNumber,
+    pgm_read_byte(&gamma8[r]),
+    pgm_read_byte(&gamma8[g]),
+    pgm_read_byte(&gamma8[b]));
 }
 
+///////////////////////////// show light /////////////////////////////
 
-////////////////// setLED  /////////////////////
-
-extern const uint8_t gamma8[];
-
-void setLED(int pixelNumber, int r, int g, int b) {
- if (r>255) {r=255;}
- if (g>255) {g=255;}
- if (b>255) {b=255;}
- //https://cdn-learn.adafruit.com/downloads/pdf/led-tricks-gamma-correction.pdf
- 
+void enoLedmapper::showLight();
+  pixels.show();
 }
 
-
-
-//LED gamma table from https://cdn-learn.adafruit.com/downloads/pdf/led-tricks-gamma-correction.pdf
-
-const uint8_t PROGMEM gamma8[] = {
- 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
- 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
- 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2,
- 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5,
- 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10,
- 10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
- 17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
- 25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
- 37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
- 51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
- 69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
- 90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
- 115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
- 144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
- 177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
- 215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
-
- ///end///
+/// end ///
