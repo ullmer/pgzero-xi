2784e4097392c14aa351f29f5154f1e0d72dc2e4
Brygg Ullmer <brygg@Bryggs-Mac-mini.local>
2023-04-17 08:45:20 -0400

    steps toward populating led calls

--- examples/arduino/arduinoEx05/enoLedmapper.cpp
+++ examples/arduino/arduinoEx05/enoLedmapper.cpp
@@ -1,138 +1,116 @@
-// Enodia ledmapper
+// Enodia LED mapper
 // Brygg Ullmer, Clemson University
 // Partial support from NSF CNS-1828611
 // Begun 2023-04
 // LLGPL3
 
-#include <stdio.h>
-#include <cstring>
-#include "enoLEDmapper.h"
+#include <Adafruit_NeoPixel.h>
+#include "enoLightmapper.h"
+#include "enoLedmapper.h"
 
-////////////////////////// constructor //////////////////////////
+///////////////////////////// constructor /////////////////////////////
 
-enoLEDmapper::enoLEDmapper(int numLEDs, int whichChain, int maxBrightVal);
-  allocateLedBuffers(numLEDs);
-  this.whichChain    = whichChain;
-  this.maxBrightVal  = maxBrightVal;
-  nextLEDmapperChunk = NULL;
+enoLedmapper::enoLedmapper(int numLights, int whichChain=0, 
+  int maxBrightVal=10) : enoLightmapper(numLights, whichChain, maxBrightVal) {
+
+  initLeds();
 }
 
-////////////////////////// allocate led buffers //////////////////////////
-
-void enoLEDmapper::allocateLedBuffers(int numLEDs) {
-  this.numLEDs = numLEDs;
-  ledKeys   = new char[numLEDs];
-  ledVals   = new char[numLEDs];
-  ledBright = new int[ numLEDs];
-  ledNames  = new const char*[numLEDs];
-}
-    
-//////////////////////////  register led //////////////////////////
-
-void enoLEDmapper::registerLED(char ledKey, char ledColorKey, int ledIdx, const char ledName[], int ledBright) {
-  if (numLEDkeysUsed >= maxLEDkeysPerChunk) {return;} // need to handle better
-
-  if (ledIdx == -1) {ledIdx = ledCursorIdx; ledCursorIdx++;}
-
-  ledKeys[ledIdx]        = ledKey;
-  ledColorKeys[ledIdx]   = ledColorKey;
-  ledNames[ledIdx]       = ledName;
-  this.ledBright[ledIdx] = ledBright;
+///////////////////////////// initiate leds /////////////////////////////
+  
+void enoLedmapper::initLeds() {
+  #if defined(NEOPIXEL_POWER)
+  // If this board has a power control pin, we must set it to output and high
+  // in order to enable the NeoPixels. We put this in an #if defined so it can
+  // be reused for other boards without compilation errors
+  pinMode(NEOPIXEL_POWER, OUTPUT);
+  digitalWrite(NEOPIXEL_POWER, HIGH);
+  #endif
+  
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.setBrightness(7); // not so bright
 }
 
-//////////////////////////  getLEDSummaryStr //////////////////////////
 
-char *enoLEDmapper::getLEDSummaryStr() {
-  int resultlen = 0;
-  char **buffer = new char*[numLEDkeysUsed];
-  char ledNameBuffer[maxLEDNameLen];
-  char *currentLine;
-  char *currentLED;
+    void setLightByIdx(int  lightIdx, char lightColor, int lightBrightness);
+    void showLight();
 
-  for (int i=0; i<numLEDkeysUsed; i++) {
-    currentLine  = new char[maxCharsPerLine];
-    int ledNameLen = strlen(ledNames[i]);
+ pixels.setPixelColor(pixelNumber,
+   pgm_read_byte(&gamma8[r]),
+   pgm_read_byte(&gamma8[g]),
+   pgm_read_byte(&gamma8[b]));
+ pixels.show();
 
-    if (ledNameLen > maxLEDNameLen) { // avoid buffer overflow error
-      currentLED = strncpy(ledNameBuffer, ledNames[i], maxLEDNameLen);
-    } else {
-      currentLED = strcpy(ledNameBuffer, ledNames[i]);
-    }
-    
-    sprintf(currentLine, "%2i %c %2X %s\n", i, ledKeys[i], ledVals[i], currentLED); //could be refined further
-    resultlen += strlen(currentLine);
-    buffer[i] = currentLine;
-  }
+  private:
+    Adafruit_NeoPixel pixels;
+};
 
-  char *result = new char[resultlen];
+//LED gamma table from https://cdn-learn.adafruit.com/downloads/pdf/led-tricks-gamma-correction.pdf
 
-  int currentCharIdx = 0;
-  for (int i=0; i<numLEDkeysUsed; i++) {
-    currentLine = buffer[i];
-    strncpy(&result[currentCharIdx], currentLine, maxCharsPerLine);
-    currentCharIdx += strlen(currentLine);
-  }
+const uint8_t PROGMEM gamma8[] = {
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
+ 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2,
+ 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5,
+ 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10,
+ 10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+ 17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+ 25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+ 37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+ 51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+ 69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+ 90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+ 115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+ 144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+ 177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+ 215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
 
-  return result;
+ ///end///
+#include <Adafruit_NeoPixel.h>
+
+#define NUMPIXELS 1
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  
+
+  Serial.begin(9600);
 }
 
-//////////////////////////  get led color by key //////////////////////////
 
-int enoLEDmapper::getLEDColByKey(char ledKey) {
-  for (int i=0; i<numLEDkeysUsed; i++) {
-    if (ledKey == ledKeys[i]) {
-      return ledColorKeys[i];
-    }
-  }
-  return 0; //probably not ideal, but a start
+////////////////// setLED  /////////////////////
+
+extern const uint8_t gamma8[];
+
+void setLED(int pixelNumber, int r, int g, int b) {
+ if (r>255) {r=255;}
+ if (g>255) {g=255;}
+ if (b>255) {b=255;}
+ //https://cdn-learn.adafruit.com/downloads/pdf/led-tricks-gamma-correction.pdf
+ 
 }
 
-//////////////////////////  get led color by index //////////////////////////
 
-int enoLEDmapper::getLEDColByIdx(int  ledIdx) {
-  return ledColorKeys[ledIdx];
-}
 
-//////////////////////////  get led idx by name //////////////////////////
+//LED gamma table from https://cdn-learn.adafruit.com/downloads/pdf/led-tricks-gamma-correction.pdf
 
-int enoLEDmapper::getLEDIdxByName(char *ledName, bool caseSensitive) {
+const uint8_t PROGMEM gamma8[] = {
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
+ 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2,
+ 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5,
+ 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10,
+ 10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+ 17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+ 25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+ 37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+ 51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+ 69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+ 90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+ 115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+ 144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+ 177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+ 215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
 
-  int cmp;
-  for (int i=0; i<numLEDkeysUsed; i++) {
-    if (caseSensitive) {cmp = strcmp(    ledName, ledNames[i]);}
-    else               {cmp = strcasecmp(ledName, ledNames[i]);}
-
-    if (cmp==0) {return i;}
-  }
-  return -1; 
-}
-
-//////////////////////////  get led idx by key //////////////////////////
-
-int enoLEDmapper::getLEDIdxByKey(char ledKey) {
-  for (int i=0; i<numLEDkeysUsed; i++) {
-    if (ledKey == ledKeys[i]) {return i;}
-  }
-  return -1;
-}
-    
-////////////////////////// set led by name //////////////////////////
-
-int enoLEDmapper::setLEDByName(char *ledName, char ledColor, int ledBrightness, bool caseSensitive) {
-  int ledIdx = getLEDIdxByName(ledName, caseSensitive);
-  setLedByIdx(ledIdx, ledColor, ledBrightness);
-}
-
-////////////////////////// set led by key //////////////////////////
-
-int enoLEDmapper::setLEDByKey(char ledKey, char ledColor, int ledBrightness) {
-  int ledIdx = getLEDIdxByKey(ledKey);
-  setLedByIdx(ledIdx, ledColor, ledBrightness);
-}
-
-////////////////////////// set led by idx //////////////////////////
-
-int enoLEDmapper::setLEDByIdx(int ledIdx, char ledColor, int ledBrightness) {
-}
-
-// end //
+ ///end///
