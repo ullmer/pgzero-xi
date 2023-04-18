141709e7ba85659fbb8ee2d224fe3e4fe4998f64
Brygg Ullmer <brygg@Bryggs-Mac-mini.local>
2023-04-17 23:54:50 -0400

    iterating

--- examples/arduino/arduinoEx05/ecslTest01.cpp
+++ examples/arduino/arduinoEx05/ecslTest01.cpp
@@ -7,10 +7,15 @@ int main() {
   enoColormapper ecm = enoColormapper();
 
   char *ecmHelp = ecm.getColorSummaryStr();
+  printf("\ncolormap summary\n");
+  printf(  "----------------\n");
   printf("%s", ecmHelp);
 
   ecsl.setLightByIdx(0, 'r', 10);
   ecsl.showLight();
+
+  printf("\nlightmap summary\n");
+  printf(  "----------------\n");
   char *ecslHelp = ecsl.getLightSummaryStr();
   printf("%s", ecslHelp);
 }
