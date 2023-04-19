648b9b23492228b0717745796b6d0705a180fbe1
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-04-19 13:49:02 -0400

    iterating on 05

---
+++ examples/arduino/arduinoEx06/arduinoEx06.ino
@@ -0,0 +1,110 @@
+// Progressive examples, toward PyGame Zero Xi (Extended Interaction) integration
+// Brygg Ullmer, Clemson University
+// Support by NSF CNS-1828611
+// Begun 2023-04
+
+#define LED0 13
+
+#ifndef ARDUINO
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "enoStrLightmapper.h"
+#include <SerialCommands.h>
+#endif
+
+#ifdef ARDUINO
+#include "enoLedmapper.h"
+#include "SerialCommands.h"
+#endif
+
+#include "enoColormapper.h"
+
+////////////////// led & serial code /////////////////////
+
+#define NUMPIXELS 1
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+int cursorIdx = 0;
+enoStrLightmapper *ecsl;
+
+char serial_command_buffer[32];
+
+/////// purple /////// 
+
+void lightPurple(SerialCommands *sender) {
+  ecsl->setLightByIdx(cursorIdx++, 'p', 20);
+
+  #ifndef ARDUINO
+  printf("purple\n");
+  char *ecslHelp = ecsl->getLightSummaryStrCompact();
+  printf("%s\n", ecslHelp); free(ecslHelp); 
+  #endif 
+}
+
+/////// orange /////// 
+
+void lightOrange( SerialCommands *sender) {
+  ecsl->setLightByIdx(cursorIdx++, 'o', 20);
+
+  #ifndef ARDUINO
+  printf("orange\n");
+  char *ecslHelp = ecsl->getLightSummaryStrCompact();
+  printf("%s\n", ecslHelp); free(ecslHelp); 
+  #endif
+}
+  
+////////////////// serial commands /////////////////////
+
+SerialCommand cmd_orange("o", lightOrange,  true);
+SerialCommand cmd_purple("p", lightPurple, true);
+SerialCommand cmd_off(   "-", lightOff,  true);
+void lightOff( SerialCommands *sender) {printf("off\n");}
+void unrecognized(SerialCommands* sender, const char* cmd) {lightOff(sender);}
+
+////////////////// setup /////////////////////
+
+void setup() {
+  #ifdef ARDUINO
+  pinMode(13, OUTPUT); // initialize digital pin 13 as an output.
+  
+  #if defined(NEOPIXEL_POWER)
+  pinMode(NEOPIXEL_POWER, OUTPUT);
+  digitalWrite(NEOPIXEL_POWER, HIGH);
+  #endif
+
+  Serial.begin(9600);
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.setBrightness(7); // not so bright
+  #endif
+
+  ecsl = new enoStrLightmapper(5);
+  enoColormapper ecm = enoColormapper();
+
+  serCmds.SetDefaultHandler(unrecognized);
+  serCmds.AddCommand(&cmd_orange);
+  serCmds.AddCommand(&cmd_purple);  
+
+  #ifndef ARDUINO
+  Stream *SerialProxy = new Stream();
+  SerialCommands serCmds(SerialProxy, serial_command_buffer, sizeof(serial_command_buffer), "\r\n", " ");
+  #endif
+}
+
+////////////////// loop/////////////////////
+
+void loop() {
+  serCmds.ReadSerial();
+}
+
+#ifndef ARDUINO
+int main() {
+  setup();
+  while (1) {
+    loop();
+    sleep(.2);
+  }
+}
+#endif
+
+/// end ///
---
+++ examples/arduino/arduinoEx06/enoLightmapper.cpp
@@ -0,0 +1,290 @@
+// Enodia lightmapper
+// Brygg Ullmer, Clemson University
+// Partial support from NSF CNS-1828611
+// Begun 2023-04
+// LLGPL3
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <cstring.h>
+#include "enoLightmapper.h"
+
+////////////////////////// constructor //////////////////////////
+
+enoLightmapper::enoLightmapper(int numLights, int whichChain, int maxBrightVal) {
+  allocateLightBuffers(numLights);
+  this->whichChain    = whichChain;
+  this->maxBrightVal  = maxBrightVal;
+  lightCursorIdx       = 0;
+  nextLightmapperChunk = 0;
+ 
+  helpHeader = "idx col brt key name\n";
+  helpTempl  =   "%2i   %1c  %2X  %1c %s\n";
+}
+
+////////////////////////// allocate light buffers //////////////////////////
+
+void enoLightmapper::allocateLightBuffers(int numLights) {
+  this->numLights = numLights;
+  lightKeys      = new char[numLights+1];
+  lightColorKeys = new char[numLights+1];
+  lightBright    = new int[ numLights];
+  lightNames  = new const char*[numLights];
+
+  for (int i=0; i<numLights; i++) {
+    lightKeys[i]      = ' ';
+    lightColorKeys[i] = ' ';
+    lightBright[i]    = 0;
+    lightNames[i]     = "";
+  }
+}
+    
+//////////////////////////  register light //////////////////////////
+
+void enoLightmapper::registerLight(char lightKey, char lightColorKey, int lightIdx, const char lightName[], int lightBright) {
+  if (lightIdx >= numLights) {return;} // need to handle better
+
+  if (lightIdx == -1) {lightIdx = lightCursorIdx; lightCursorIdx++;}
+
+  lightKeys[lightIdx]        = lightKey;
+  lightColorKeys[lightIdx]   = lightColorKey;
+  lightNames[lightIdx]       = lightName;
+  this->lightBright[lightIdx] = lightBright;
+}
+
+//////////////////////////  getLightSummaryStr //////////////////////////
+
+char *enoLightmapper::getLightSummaryStrCompact() {
+  int i, endVal = numLights*2+2;
+  char *resultStr = new char[endVal+1];
+
+  for (i=0; i<numLights; i++) {
+    resultStr[i] = lightColorKeys[i];	    
+  }
+
+  resultStr[i++] = '\r';
+  resultStr[i++] = '\n';
+
+  for (;i<endVal; i++) {resultStr[i] = '.';}
+  resultStr[endVal] = 0; 
+
+  return resultStr;
+}
+
+//////////////////////////  getLightSummaryStr //////////////////////////
+
+char *enoLightmapper::getLightSummaryStr() {
+  int resultlen = 0;
+
+  int bufflen = numLights;
+  if (showHelpHeader) {bufflen++;}
+
+  char *buffer[bufflen];
+  char lightNameBuffer[maxLightNameLen];
+  char *currentLine;
+  char *currentLight;
+  int  buffIdx = 0;
+  char *helpHeaderHandle;
+
+  if (showHelpHeader) {
+    helpHeaderHandle = strdup(helpHeader); // workaround casting issue
+    buffer[0] = helpHeaderHandle;
+    resultlen += strlen(buffer[0]);
+    buffIdx += 1;
+  }
+  for (int i=0; i<numLights; i++) {
+    char *currentLine = new char[maxCharsPerLine];
+    int lightNameLen = strlen(lightNames[i]);
+
+    if (lightNameLen > maxLightNameLen) {
+      // buffer overflow errors are problematic 
+      currentLight = strncpy(lightNameBuffer, lightNames[i], 
+                             maxLightNameLen);
+    } else {
+      currentLight = strcpy(lightNameBuffer, lightNames[i]);
+    }
+    
+    //helpHeader = "idx col brt key name\n";
+    //helpTempl  =   "%2i   %1c  %2X  %1c %s\n";
+
+    sprintf(currentLine, helpTempl, i, lightColorKeys[i], lightBright[i], 
+            lightKeys[i], lightNames[i]);
+
+    resultlen += strlen(currentLine);
+    buffer[buffIdx] = currentLine;
+    buffIdx++;
+  }
+
+  char *result = new char[resultlen+1];
+  int currentCharIdx = 0;
+
+  for (int i=0; i<buffIdx; i++) {
+    currentLine = buffer[i];
+    int currentLineLen = strlen(currentLine);
+    
+    int bytesToCopy;
+    if (currentLineLen < maxCharsPerLine) { bytesToCopy = currentLineLen;}
+    else                                  { bytesToCopy = maxCharsPerLine;}
+    memcpy(&(result[currentCharIdx]), currentLine, bytesToCopy);
+    currentCharIdx += bytesToCopy;
+  }
+  result[resultlen]=0;
+
+  if (helpHeaderHandle != 0) {free(helpHeaderHandle);} 
+
+  return result;
+}
+
+//////////////////////////  get light color by key //////////////////////////
+
+int enoLightmapper::getLightIdxByKey(char lightKey) {
+  for (int i=0; i<numLights; i++) {
+    if (lightKey == lightKeys[i]) {
+      return i;
+    }
+  }
+  return -1; //probably not ideal, but a start
+}
+
+//////////////////////  get light color by key -- multiple returns ///////////
+
+IntArr enoLightmapper::getLightIdxByKeyMulti(char lightKey) {
+
+  int keyCount = 0;
+
+  for (int i=0; i<numLights; i++) {
+    if (lightKey == lightKeys[i]) {
+      keyCount++;
+    }
+  }
+
+  if (keyCount == 0) {return 0;} //probably not ideal, but a start
+
+  IntArr iaResult = IntArr(keyCount);
+
+  int keyIdx = 0;
+
+  for (int i=0; i<numLights; i++) {
+    if (lightKey == lightKeys[i]) {
+      iaResult.setEl(keyIdx, i); keyIdx++;
+    }
+  }
+
+  return iaResult;
+}
+
+//////////////////////////  get light color by key //////////////////////////
+
+int enoLightmapper::getLightColByKey(char lightKey) {
+  int lightIdx = getLightIdxByKey(lightKey);
+  if (lightIdx < 0) {return lightIdx;}
+  int color = lightColorKeys[lightIdx];
+  return color; 
+}
+
+//////////////////////////  get light color by index //////////////////////////
+
+int enoLightmapper::getLightColByIdx(int  lightIdx) {
+  return lightColorKeys[lightIdx];
+}
+
+//////////////////////////  get light idx by name //////////////////////////
+
+int enoLightmapper::getLightIdxByName(char *lightName, bool caseSensitive) {
+
+  int cmp;
+  for (int i=0; i<numLights; i++) {
+    if (caseSensitive) {cmp = strcmp(    lightName, lightNames[i]);}
+    else               {cmp = strcasecmp(lightName, lightNames[i]);}
+
+    if (cmp==0) {return i;}
+  }
+  return -1; 
+}
+
+//////////////////////////  get light idx by key //////////////////////////
+
+//int enoLightmapper::getLightIdxByKey(char lightKey) {
+//  for (int i=0; i<numLights; i++) {
+//    if (lightKey == lightKeys[i]) {return i;}
+//  }
+//  return -1;
+//}
+    
+////////////////////////// set light by name //////////////////////////
+
+void enoLightmapper::setLightByName(char *lightName, char lightColor, int lightBrightness, bool caseSensitive) {
+  int lightIdx = getLightIdxByName(lightName, caseSensitive);
+  setLightByIdx(lightIdx, lightColor, lightBrightness);
+}
+
+////////////////////////// set light by key //////////////////////////
+
+void enoLightmapper::setLightByKey(char lightKey, char lightColor, int lightBrightness, bool multikey) {
+  if (!multikey) {
+    int lightIdx = getLightIdxByKey(lightKey);
+    setLightByIdx(lightIdx, lightColor, lightBrightness);
+
+  } else {
+
+    IntArr lightIdxs = getLightIdxByKeyMulti(lightKey);
+    int liLen = lightIdxs.getSize();
+    for (int i; i<liLen; i++) {
+      int idx = lightIdxs[i];
+      setLightByIdx(idx, lightColor, lightBrightness);
+    }
+  } 
+}
+
+////////////////////////// set light by idx //////////////////////////
+
+//int enoLightmapper::setLightByIdx(int lightIdx, char lightColor, int lightBrightness) {
+//}
+
+/////////////////////////////////////////////////////////////////////////
+///////////////////////////// integer array /////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+///////////////////////////// constructor /////////////////////////////
+
+IntArr::IntArr(int arrSize) {
+  intArr = new int[arrSize];
+  this->arrSize = arrSize;
+}
+
+///////////////////////////// getSize /////////////////////////////
+
+int IntArr::getSize() {return arrSize;}
+  
+///////////////////////////// set elements /////////////////////////////
+
+int IntArr::setEl(int idx, int val) {
+  if (idx > arrSize) {return -1;}
+  intArr[idx] = val;
+  return 1;
+}
+
+int IntArr::setEls(int *array, int listLen) {
+  if (listLen > arrSize) {return -1;}
+  for (int i=0; i<listLen; i++) {
+    intArr[i] = array[i];
+  }
+
+  return 1;
+}
+
+///////////////////////////// get elements /////////////////////////////
+
+int IntArr::getEl(int idx) {
+  if (idx >= arrSize) {return -1;} // consider an alternate implementation
+  return intArr[idx];
+}
+
+int IntArr::operator[](int idx) {
+
+  if (idx >= arrSize) {return -1;} // consider an alternate implementation
+  return intArr[idx];
+}
+
+/// end ///
