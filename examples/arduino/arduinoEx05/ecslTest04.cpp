0a6852a076050139d64a7cc60d46cd477281f054
Brygg Ullmer <ullmer@acm.org>
2023-04-19 13:30:02 -0400

    iterating

--- examples/arduino/arduinoEx05/ecslTest04.cpp
+++ examples/arduino/arduinoEx05/ecslTest04.cpp
@@ -10,15 +10,13 @@
 #include "enoColormapper.h"
 #include "SerialCommands.h"
 
-
-
 ////////////////// led & serial code /////////////////////
 
 char serial_command_buffer[32];
 Stream *SerialProxy = new Stream();
 SerialCommands serCmds(SerialProxy, serial_command_buffer, sizeof(serial_command_buffer), "\r\n", " ");
 
-int cursorIdx = 2;
+int cursorIdx = 0;
 enoStrLightmapper *ecsl;
 
 /////// purple /////// 
@@ -48,9 +46,9 @@ void lightOrange( SerialCommands *sender) {
 void lightOff( SerialCommands *sender) {printf("off\n");}
 void unrecognized(SerialCommands* sender, const char* cmd) {lightOff(sender);}
 
-SerialCommand cmd_orange( "o", lightOrange,  true);
-SerialCommand cmd_purple("b", lightPurple, true);
-SerialCommand cmd_off( "-", lightOff,  true);
+SerialCommand cmd_orange("o", lightOrange,  true);
+SerialCommand cmd_purple("p", lightPurple, true);
+SerialCommand cmd_off(   "-", lightOff,  true);
 
 ////////////////// main /////////////////////
 
